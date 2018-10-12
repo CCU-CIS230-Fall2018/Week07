330124a831763098d15722bb2568d8d318c04bc6
Noah Jamison <njamison@students.ccu.edu>
2018-10-11 23:17:39 -0600

    Finished.

---
+++ Week7Threading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7Threading
+{
+    class Program
+    {
+        static void Main()
+        {
+            string message = "Doing The Thing";
+            var tooLong = new TakesTooLong();
+            tooLong.DoTheThing();
+
+            while (1 == 1)
+            {
+                Console.WriteLine(message);
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ Week7Threading/Week7Threading/TakesTooLong.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7Threading
+{
+    public class TakesTooLong
+    {
+        public async Task DoTheThing()
+        {
+            await Task.Run(() =>
+            {
+                TimeWaster();
+            });
+        }
+
+        public static async Task<string> TimeWaster()
+        {
+            int fakeList;
+
+            // Let code run until fakeList reaches it limit to see how
+            // it is made to run asynchronus.
+            for (fakeList = 0; fakeList < 15; fakeList++)
+            {
+                Console.WriteLine(fakeList);
+                System.Threading.Thread.Sleep(700);
+            }
+
+            return "Counter = " + fakeList;
+        }
+    }
+}
